4bc3a4842dc2de4cb966fdd5000544f5636990e0
Fadly Chairurrahman <93429956+fadlychairurrahman@users.noreply.github.com>
2025-11-03 05:01:13 +0700

    fix: koneksi db railway

--- db/koneksi.js
+++ db/koneksi.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql2');
 
-const koneksi = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', 
-  database: 'admin_toko'
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,      // pakai env
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
-koneksi.connect(err => {
-  if (err) throw err;
-  console.log('Terhubung ke database MySQL');
+db.connect((err) => {
+  if (err) {
+    console.error('❌ Koneksi database gagal:', err.message);
+  } else {
+    console.log('✅ Terhubung ke database MySQL');
+  }
 });
 
-module.exports = koneksi;
+module.exports = db;
