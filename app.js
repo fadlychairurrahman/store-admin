424c2a4699fd4a78dc4db65dc50157d59507be0d
Fadly Chairurrahman <93429956+fadlychairurrahman@users.noreply.github.com>
2025-11-03 04:53:40 +0700

    fix: set start script and PORT for Railway

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
 });
 
 // Jalankan server
-app.listen(3000, () => {
-  console.log('🚀 Server berjalan di http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server berjalan di port ${PORT}`);
 });
